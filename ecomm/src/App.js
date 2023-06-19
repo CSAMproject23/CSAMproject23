a4f1bac4bc722af09f94811bc16fed4640e7956f
Pardaman Kaur <pardamankhokhar@gmail.com>
2023-06-19 03:00:59 -0400

    login and register page

---
+++ ecomm/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Homepage from "./component/homepage/homepage";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
+function App() {
+  return (
+    
+      <div className="App"> 
+      {/* <Homepage/> */}
+      {/* <Login/> */}
+      <Register/>
+      </div>
+    
+  );
+}
+
+export default App;
